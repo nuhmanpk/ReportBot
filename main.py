3b9fc750ad69e94cbffbb58624693bed9abc9d6b
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-10-18 12:23:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import pyrogram
 import os
 from pyrogram import Client, filters
@@ -16,28 +17,36 @@ async def start(bot, update):
     await update.reply_text("Start Message Here")
 
 
-@bughunter0.on_message((filters.command(["report"]) | filters.regex("@admins") | filters.regex("@admin")) & filters.group)
+@bughunter0.on_message(
+    (filters.command(["report"]) | filters.regex("@admins") | filters.regex("@admin"))
+    & filters.group
+)
 async def report(bot, message):
- if message.reply_to_message:
-    chatid = message.chat.id
-    reporter = message.from_user.id
-    admins = await bot.get_chat_members(chat_id=chatid, filter="administrators")
-    success=0
-    for admin in admins:
-        try:
-            success +=1
-            await message.reply_to_message.copy(f"{admin.user.id}")
-            await bot.send_message(
-                text=f""" Reporter : {reporter} \n 
-                Message Link : {message.reply_to_message.link}""",
-                chat_id=admin.user.id,
-            )
-        except:
-            pass
-    if success > 0 :
-        await message.reply_text("Reported to Admins")
+    if message.reply_to_message:
+        chatid = message.chat.id
+        reporter = message.from_user.id
+        mention = message.from_user.mention
+        admins = await bot.get_chat_members(chat_id=chatid, filter="administrators")
+        success = 0
+        for admin in admins:
+            try:
 
- else:
-    return
+                await bot.send_message(
+                    text=f""" 
+Reporter : {mention}({reporter}) \n 
+Message Link : {message.reply_to_message.link}\n Reported Message 👇""",
+                    chat_id=admin.user.id,
+                )
+                await message.reply_to_message.copy(f"{admin.user.id}")
+                success += 1
+            except:
+                pass
+        if success > 0:
+            await message.reply_text("Reported to Admins")
+
+    else:
+        return
+
+
 bughunter0.run()
 
